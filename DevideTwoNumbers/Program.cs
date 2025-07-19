91515966ea1ee5c34d58b5c3d4f1e08725ac253d
ShirinMahbuba <mahbubasara3@gmail.com>
2025-07-19 22:51:20 +0600

    Add project files.

---
+++ DevideTwoNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevideTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"First Number:");
+            string input1 = Console.ReadLine();
+            double num1;
+            if (!double.TryParse(input1, out num1))
+            {
+             Console.WriteLine("Invalid input! please input a valid number");
+                return;
+            }
+
+            Console.WriteLine($"Secound Number:");
+            string input2 = Console.ReadLine();
+            double num2;
+
+            if(!double.TryParse(input2,out num2))
+            {
+                Console.WriteLine("Invalid input!please input a valid number");
+                return;
+            }
+
+            if(num2 == 0)
+            {
+                Console.WriteLine("cannot devide by zero");
+                return;
+            }
+            double result = num1 / num2;
+            Console.WriteLine($"Result : {num1}/{num2}={result}");
+
+            
+        }
+    }
+}
